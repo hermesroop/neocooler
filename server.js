d38a54941bd2497928625119b6fb3a6cdc765b66
hermesroop <hermesroop@outlook.com>
2026-05-12 19:04:57 -0300

    server.js

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const PLACE_ID = process.env.PLACE_ID;
 
+app.get("/", (req, res) => {
+  res.send("API online");
+});
+
 app.get("/google-reviews", async (req, res) => {
   try {
-    const response = await fetch(`https://places.googleapis.com/v1/places/${PLACE_ID}`, {
-      headers: {
-        "X-Goog-Api-Key": GOOGLE_API_KEY,
-        "X-Goog-FieldMask": "displayName,rating,userRatingCount,reviews"
+    const response = await fetch(
+      `https://places.googleapis.com/v1/places/${PLACE_ID}`,
+      {
+        headers: {
+          "X-Goog-Api-Key": GOOGLE_API_KEY,
+          "X-Goog-FieldMask":
+            "displayName,rating,userRatingCount,reviews"
+        }
       }
-    });
+    );
 
     const data = await response.json();
 
@@ -24,8 +33,13 @@ app.get("/google-reviews", async (req, res) => {
       total: data.userRatingCount || 0,
       avaliacoes: data.reviews || []
     });
+
   } catch (error) {
-    res.status(500).json({ erro: "Erro ao buscar avaliações" });
+    console.log(error);
+
+    res.status(500).json({
+      erro: "Erro ao buscar avaliações"
+    });
   }
 });
 
